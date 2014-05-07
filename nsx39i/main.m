b292586c52d4a9dd718456e89339c9f633f0b0ab
nya <home@cathand.org>
2014-05-07 12:19:08 +0900

    play inet coop saitama

--- nsx39i/main.m
+++ nsx39i/main.m
@@ -12,6 +12,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#import "pronounce_table.h"
+
 
 #define DEVICE_NAME CFSTR("NSX-39 ")
 #define PORT_NAME CFSTR("NSX-39voiceOut")
@@ -78,6 +80,74 @@ void sendNote(MIDIEndpointRef endPoint, Byte *message, UInt16 length)
     
     MIDISend(getOutPutPort(), endPoint, &packetList);
 }
+
+BOOL sendPronounce(MIDIEndpointRef endPoint, NSString *str) {
+	int num = GetPronounceNumber(str);
+	if (num >= 0) {
+		// F0 43 79 09 11 0A 00 ** F7
+		Byte msg[9];
+		msg[0] = 0xF0;
+		msg[1] = 0x43;
+		msg[2] = 0x79;
+		msg[3] = 0x09;
+		msg[4] = 0x11;
+		msg[5] = 0x0A;
+		msg[6] = 0x00;
+		msg[7] = num;
+		msg[8] = 0xF7;
+		sendNote(endPoint, msg, 9);
+		return YES;
+	} else {
+		return NO;
+	}
+}
+
+void sendVoice(MIDIEndpointRef endPoint, NSString *pronounce, int scale, float duration, float base) {
+	Byte msg[3];
+
+	if (sendPronounce(endPoint, pronounce)) {
+		msg[0] = 0x90;
+		msg[1] = scale;
+		msg[2] = 0x64;
+	} else {
+		msg[0] = 0xB0;
+		msg[1] = 0x78;
+		msg[2] = 0x00;
+	}
+	sendNote(endPoint, msg, 3);
+	[NSThread sleepForTimeInterval:base * duration];
+}
+
+#if 1
+
+int main(int argc, char **argv)
+{
+    CFStringRef endPointName = DEVICE_NAME;
+    MIDIEndpointRef endPoint = getEndpointWithDisplayName(endPointName);
+    if (endPoint == 0) {
+        fprintf(stdout, "%s not found.\n", CFStringGetCStringPtr(DEVICE_NAME, kCFStringEncodingASCII));
+        return -1;
+    }
+	
+	float base = 0.5;
+	sendVoice(endPoint, @"あ", 0x43, 1.0 / 2.0, base);
+	sendVoice(endPoint, @"い", 0x43, 1.0 / 2.0, base);
+	sendVoice(endPoint, @"ね", 0x43, 1.0 / 2.0, base);
+	sendVoice(endPoint, @"と", 0x43, 1.0 / 2.0, base);
+	sendVoice(endPoint, @"こ", 0x48, 1.0, base);
+	sendVoice(endPoint, @"ぷ", 0x45, 1.0, base);
+	sendVoice(endPoint, @"", 0x3c, 1.0, base);
+	sendVoice(endPoint, @"さ", 0x43, 1.0 / 2.0, base);
+	sendVoice(endPoint, @"い", 0x43, 1.0 / 2.0, base);
+	sendVoice(endPoint, @"た", 0x3c, 1.0 / 2.0, base);
+	sendVoice(endPoint, @"ま", 0x3c, 1.0 / 2.0, base);
+	sendVoice(endPoint, @"", 0x3c, 0, base);
+	
+    MIDIEndpointDispose(endPoint);
+    return 0;
+}
+
+#else
 
 int main(int argc, char **argv)
 {
@@ -192,3 +262,5 @@ MIDI_ENDPOINT_DISPOSE:
     
     return 0;
 }
+
+#endif
---
+++ nsx39i/pronounce_table.h
@@ -0,0 +1,19 @@
+//
+//  pronounce_table.h
+//  nsx39i
+//
+//  Created by nya on 2014/05/07.
+//  Copyright (c) 2014年 semnil. All rights reserved.
+//
+
+#ifndef nsx39i_pronounce_table_h
+#define nsx39i_pronounce_table_h
+
+
+#import <Foundation/Foundation.h>
+
+
+int GetPronounceNumber(NSString *noun);
+
+
+#endif
